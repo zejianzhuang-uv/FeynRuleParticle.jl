b4e351046532ff42c0186fe168306112e6468240
zejianzhuang-uv <zejian.zhuang@uv.es>
2026-04-04 23:14:15 +0200

    Export write_couple_channel function from FeynRuleParticle module

--- src/FeynRuleParticle.jl
+++ src/FeynRuleParticle.jl
@@ -1,6 +1,6 @@
 module FeynRuleParticle
 
-export write_field_info, write_params_info, write_particle_quantum_number_info, couple_channel, write_particle_isospin_multiplet
+export write_field_info, write_params_info, write_particle_quantum_number_info, couple_channel, write_particle_isospin_multiplet, write_couple_channel
 
 include("./FeynRule_info.jl")
 include("./particle_quantum_number.jl")
