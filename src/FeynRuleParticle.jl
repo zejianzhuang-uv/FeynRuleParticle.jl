5107b096cf28438e0d012d2d69cfd3062721a653
zejianzhuang-uv <zejian.zhuang@uv.es>
2026-07-17 12:38:38 +0200

    Add scattering_channel_MMA function for enhanced particle pairing and grouping

--- src/FeynRuleParticle.jl
+++ src/FeynRuleParticle.jl
@@ -1,6 +1,6 @@
 module FeynRuleParticle
 
-export write_field_info, write_params_info, write_particle_quantum_number_info, couple_channel, write_particle_isospin_multiplet, write_couple_channel, scattering_channel
+export write_field_info, write_params_info, write_particle_quantum_number_info, couple_channel, write_particle_isospin_multiplet, write_couple_channel, scattering_channel, scattering_channel_MMA
 
 include("./FeynRule_info.jl")
 include("./particle_quantum_number.jl")
